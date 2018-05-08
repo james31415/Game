33bee36c5578e0a501776a0297bdf6dcd2cd1355
Samuel Archer <desearcher@gmail.com>
2018-05-08 09:36:23 -0400

    updated logging code, joystick code, keyboard code, etc

--- main.go
+++ main.go
@@ -1,23 +1,83 @@
 package main
 
 import (
+    "fmt" // for log
     "github.com/dradtke/go-allegro/allegro"
     "github.com/dradtke/go-allegro/allegro/audio"
     "github.com/dradtke/go-allegro/allegro/acodec"
     //"github.com/yuin/gopher-lua"
-    "os"
+    "os" // for loadSound check
 )
 
 const (
+    LOGGING = false
+    LOOPING = true
     // SOUND
     PIANO_WAV = "dat/snd/piano.wav"
     // DISPLAY
     WINX = 800
     WINY = 600
     // MAIN
-    FPS  = 1
+    FPS  = 30
 )
 
+// dump Joystick information, if present
+func checkJoysticks() {
+    log("CONFIGURING JOYSTICKS")
+    if allegro.ReconfigureJoysticks() {
+        log(" = PASSED")
+    } else {
+        log(" = FAILED")
+        return
+    }
+    // TODO // Too Deep
+    if joys := allegro.NumJoysticks(); joys > 0 {
+        log(" = JOYS:", joys)
+        for joy := 0; joy < joys; joy++ {
+            if joystick, err := allegro.GetJoystick(joy); err != nil {
+                log(err)
+            } else {
+                log(" = JOYSTICK:", joy)
+                log(" = NAME:", joystick.Name())
+                if sticks := joystick.NumSticks(); sticks > 0 {
+                    log(" = STICKS:", sticks)
+                    for stick := 0; stick < sticks; stick++ {
+                        if stickName, err := joystick.StickName(stick); err != nil {
+                            log(err)
+                        } else {
+                            log(" = STICK:", stick)
+                            log(" = NAME:", stickName)
+                            if axes := joystick.NumAxes(stick); axes > 0 {
+                                log(" = AXES:", axes)
+                                for axis := 0; axis < axes; axis++ {
+                                    if axisName, err := joystick.AxisName(stick, axis); err != nil {
+                                        log(err)
+                                    } else {
+                                        log(" = AXIS:", axis)
+                                        log(" = NAME:", axisName)
+                                    }
+                                }
+                            }
+                            if nbutt := joystick.NumButtons(); nbutt > 0 {
+                                log(" = NBUTT:", nbutt)
+                                for butt := 0; butt < nbutt; butt++ {
+                                    if buttName, err := joystick.ButtonName(stick); err != nil {
+                                        log(err)
+                                    } else {
+                                        log(" = BUTT:", butt)
+                                        log(" = NAME:", buttName)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// temporary, until sprites are implemented and can load their own assets
 func loadSound(filename string) (instance *audio.SampleInstance, err error) {
     if _, err = os.Stat(filename); os.IsNotExist(err) {
         return nil, err
@@ -31,33 +91,66 @@ func loadSound(filename string) (instance *audio.SampleInstance, err error) {
     return instance, nil
 }
 
-// defer logging utility function while debugging
-func logFunc(msg string, function func()) {
-    println(msg)
-    function()
-}
+func log(msg ...interface{}) { if LOGGING { fmt.Println(msg...) } }
+func logFunc(msg string, function func()) { log(msg); function() }
 
 func main() {
+    log("STARTING..."); defer log("...FINISHED")
     allegro.Run(func() {
         var (
             display    *allegro.Display
             event       allegro.Event
             eventQueue *allegro.EventQueue
+            joyState    allegro.JoystickState
+            keyState    allegro.KeyboardState
             timer      *allegro.Timer
-            running    bool = true
+            running    bool = LOOPING
             err        error
         )
+
+        /* /////////////////
+        ** Configure Display
+        */ /////////////////
+        log("CONFIG DISPLAY")
+        var flags allegro.DisplayFlags = 0
+
+        // Manually set DisplayFlags for now
+        // planning on setting with a config
+
+           flags |= allegro.WINDOWED
+        // flags |= allegro.FULLSCREEN
+        // flags |= allegro.OPENGL
+        // flags |= allegro.RESIZABLE
+        // flags |= allegro.FRAMELESS
+        // flags |= allegro.NOFRAME
+           flags |= allegro.GENERATE_EXPOSE_EVENTS
+        // flags |= allegro.OPENGL_3_0
+        // flags |= allegro.OPENGL_FORWARD_COMPATIBLE
+        // flags |= allegro.FULLSCREEN_WINDOW
+
+        if flags == 0 {            log(" = DEFAULT"                  ) } else {
+            if flags & 0x001 > 0 { log(" = WINDOWED"                 ) }
+            if flags & 0x002 > 0 { log(" = FULLSCREEN"               ) }
+            if flags & 0x004 > 0 { log(" = OPENGL"                   ) }
+            if flags & 0x010 > 0 { log(" = RESIZABLE"                ) }
+            if flags & 0x020 > 0 { log(" = FRAMELESS"                ) } // FRAMELESS == NOFRAME
+            if flags & 0x020 > 0 { log(" = NOFRAME"                  ) } // NOFRAME == FRAMELESS
+            if flags & 0x040 > 0 { log(" = GENERATE_EXPOSE_EVENTS"   ) }
+            if flags & 0x080 > 0 { log(" = OPENGL_3_0"               ) }
+            if flags & 0x100 > 0 { log(" = OPENGL_FORWARD_COMPATIBLE") }
+            if flags & 0x200 > 0 { log(" = FULLSCREEN_WINDOW"        ) }
+        }
 
         /* ///////////////////////////
         ** Install Audio & Audio Codec
         */ ///////////////////////////
-        println("INSTALL AUDIO")
+        log("INSTALL AUDIO")
         if err = audio.Install(); err != nil {
             panic(err)
         } else {
             defer logFunc("UNINSTALL AUDIO AND AUDIO CODEC", audio.Uninstall)
             audio.ReserveSamples(1)
-            println("INSTALL AUDIO CODEC")
+            log("INSTALL AUDIO CODEC")
             if err = acodec.Install(); err != nil {
                 panic(err)
             }
@@ -66,7 +159,7 @@ func main() {
         /* //////////////////
         ** Create Event Queue
         */ //////////////////
-        println("CREATE EVENT QUEUE")
+        log("CREATE EVENT QUEUE")
         if eventQueue, err = allegro.CreateEventQueue(); err != nil {
             panic(err)
         } else {
@@ -76,33 +169,7 @@ func main() {
         /* ////////////////
         ** Register Display
         */ ////////////////
-        println("CREATE DISPLAY")
-        var flags allegro.DisplayFlags = 0
-
-        // flags |= allegro.WINDOWED
-        // flags |= allegro.FULLSCREEN
-        // flags |= allegro.OPENGL
-        // flags |= allegro.RESIZABLE
-        // flags |= allegro.FRAMELESS
-        // flags |= allegro.NOFRAME
-        // flags |= allegro.GENERATE_EXPOSE_EVENTS
-        // flags |= allegro.OPENGL_3_0
-        // flags |= allegro.OPENGL_FORWARD_COMPATIBLE
-        // flags |= allegro.FULLSCREEN_WINDOW
-
-        if flags == 0 {            println(" = DEFAULT"                  ) } else {
-            if flags & 0x001 > 0 { println(" = WINDOWED"                 ) }
-            if flags & 0x002 > 0 { println(" = FULLSCREEN"               ) }
-            if flags & 0x004 > 0 { println(" = OPENGL"                   ) }
-            if flags & 0x010 > 0 { println(" = RESIZABLE"                ) }
-            if flags & 0x020 > 0 { println(" = FRAMELESS"                ) }
-            if flags & 0x020 > 0 { println(" = NOFRAME"                  ) }
-            if flags & 0x040 > 0 { println(" = GENERATE_EXPOSE_EVENTS"   ) }
-            if flags & 0x080 > 0 { println(" = OPENGL_3_0"               ) }
-            if flags & 0x100 > 0 { println(" = OPENGL_FORWARD_COMPATIBLE") }
-            if flags & 0x200 > 0 { println(" = FULLSCREEN_WINDOW"        ) }
-        }
-
+        log("CREATE DISPLAY")
         allegro.SetNewDisplayFlags(flags)
         if display, err = allegro.CreateDisplay(WINX,WINY); err != nil {
             panic(err)
@@ -116,18 +183,19 @@ func main() {
         /* /////////////////
         ** Register Joystick
         */ /////////////////
-        println("INSTALL JOYSTICK")
+        log("INSTALL JOYSTICK")
         if err = allegro.InstallJoystick(); err != nil {
             panic(err)
         } else {
             defer logFunc("UNINSTALL JOYSTICK", allegro.UninstallJoystick)
             eventQueue.RegisterEventSource(allegro.JoystickEventSource())
+            checkJoysticks()
         }
         
         /* /////////////////
         ** Register Keyboard
         */ /////////////////
-        println("INSTALL KEYBOARD")
+        log("INSTALL KEYBOARD")
         if err = allegro.InstallKeyboard(); err != nil {
             panic(err)
         } else {
@@ -142,7 +210,7 @@ func main() {
         /* //////////////
         ** Register Mouse
         */ //////////////
-        println("INSTALL MOUSE")
+        log("INSTALL MOUSE")
         if err = allegro.InstallMouse(); err != nil {
             panic(err)
         } else {
@@ -157,7 +225,7 @@ func main() {
         /* //////////////
         ** Register Timer
         */ //////////////
-        println("CREATE TIMER")
+        log("CREATE TIMER")
         if timer, err = allegro.CreateTimer(1.0 / FPS); err != nil {
             panic(err)
         } else {
@@ -166,13 +234,6 @@ func main() {
             timer.Start()
         }
 
-        /* ////////////////////
-        ** Set Solid Background
-        */ ////////////////////
-        allegro.ClearToColor(allegro.MapRGB(32,64,96))
-        allegro.FlipDisplay()
-        allegro.ClearToColor(allegro.MapRGB(96,64,32))
-
         /* //////////
         ** Sound Test
         */ //////////
@@ -180,37 +241,64 @@ func main() {
         if err != nil { panic(err) }
         piano.Play()
 
-        /* /////////
-        ** Main Loop
-        */ /////////
-        for running {
-            switch e := eventQueue.WaitForEvent(&event); e.(type) {
-            case allegro.DisplayCloseEvent:          println("DisplayCloseEvent"         ); running = false
-            case allegro.DisplayExposeEvent:         println("DisplayExposeEvent"        );
-            case allegro.DisplayFoundEvent:          println("DisplayFoundEvent"         );
-            case allegro.DisplayLostEvent:           println("DisplayLostEvent"          );
-            case allegro.DisplayOrientationEvent:    println("DisplayOrientationEvent"   );
-            case allegro.DisplayResizeEvent:         println("DisplayResizeEvent"        );
-            case allegro.DisplaySwitchInEvent:       println("DisplaySwitchInEvent"      );
-            case allegro.DisplaySwitchOutEvent:      println("DisplaySwitchOutEvent"     );
-            case allegro.JoystickAxisEvent:          println("JoystickAxisEvent"         );
-            case allegro.JoystickButtonDownEvent:    println("JoystickButtonDownEvent"   );
-            case allegro.JoystickButtonUpEvent:      println("JoystickButtonUpEvent"     );
-            case allegro.JoystickConfigurationEvent: println("JoystickConfigurationEvent");
-            case allegro.KeyCharEvent:               println("KeyCharEvent"              );
-            case allegro.KeyDownEvent:               println("KeyDownEvent"              );
-            case allegro.KeyUpEvent:                 println("KeyUpEvent"                );
-            case allegro.MouseAxesEvent:             println("MouseAxesEvent"            );
-            case allegro.MouseButtonDownEvent:       println("MouseButtonDownEvent"      );
-            case allegro.MouseButtonUpEvent:         println("MouseButtonUpEvent"        );
-            case allegro.MouseEnterDisplayEvent:     println("MouseEnterDisplayEvent"    );
-            case allegro.MouseLeaveDisplayEvent:     println("MouseLeaveDisplayEvent"    );
-            case allegro.MouseWarpedEvent:           println("MouseWarpedEvent"          );
-            case allegro.TimerEvent:                 print(  "TimerEvent: "              ); println(timer.Count())
-            case allegro.UserEvent:                  println("UserEvent"                 );
-            default:                                 print(  "UnknownEvent: "            ); println(e)
+        /* ///////// // /////////////////////
+        ** Main Loop // Damn Huge Switch Loop
+        */ ///////// // /////////////////////
+        log("LOOPING...")
+        quit := func() { log("QUITING..."); running = false }
+        for running { switch e := eventQueue.WaitForEvent(&event); e.(type) {
+
+            // Display Events
+            case allegro.DisplayCloseEvent:          log("DisplayCloseEvent"         ); quit()
+            case allegro.DisplayExposeEvent:         log("DisplayExposeEvent"        ); allegro.FlipDisplay()
+            case allegro.DisplayFoundEvent:          log("DisplayFoundEvent"         );
+            case allegro.DisplayLostEvent:           log("DisplayLostEvent"          );
+            case allegro.DisplayOrientationEvent:    log("DisplayOrientationEvent"   );
+            case allegro.DisplayResizeEvent:         log("DisplayResizeEvent"        ); display.AcknowledgeResize()
+            case allegro.DisplaySwitchInEvent:       log("DisplaySwitchInEvent"      );
+            case allegro.DisplaySwitchOutEvent:      log("DisplaySwitchOutEvent"     );
+
+            // Joystick Events
+            case allegro.JoystickAxisEvent:          log("JoystickAxisEvent"         ); joyState.Get()
+            case allegro.JoystickButtonDownEvent:    log("JoystickButtonDownEvent"   ); joyState.Get()
+            case allegro.JoystickButtonUpEvent:      log("JoystickButtonUpEvent"     ); joyState.Get()
+            case allegro.JoystickConfigurationEvent: log("JoystickConfigurationEvent"); checkJoysticks()
+
+            // Keyboard Events
+            case allegro.KeyCharEvent:               log("KeyCharEvent"              ); keyState.Get()
+                switch {
+                case keyState.IsDown(allegro.KEY_ESCAPE): quit()
+                case keyState.IsDown(allegro.KEY_Q     ): quit()
+                case keyState.IsDown(allegro.KEY_UP    ): log(" = UP"   );
+                case keyState.IsDown(allegro.KEY_DOWN  ): log(" = DOWN" );
+                case keyState.IsDown(allegro.KEY_LEFT  ): log(" = LEFT" );
+                case keyState.IsDown(allegro.KEY_RIGHT ): log(" = RIGHT");
+                }
+            case allegro.KeyDownEvent:               log("KeyDownEvent"              ); allegro.FlipDisplay(); allegro.FlipDisplay()
+            case allegro.KeyUpEvent:                 log("KeyUpEvent"                ); // Flip-flop background color for testing
+
+            // Mouse Events
+            case allegro.MouseAxesEvent:             log("MouseAxesEvent"            );
+            case allegro.MouseButtonDownEvent:       log("MouseButtonDownEvent"      );
+            case allegro.MouseButtonUpEvent:         log("MouseButtonUpEvent"        );
+            case allegro.MouseEnterDisplayEvent:     log("MouseEnterDisplayEvent"    ); timer.Start() // Pauses when mouse
+            case allegro.MouseLeaveDisplayEvent:     log("MouseLeaveDisplayEvent"    ); timer.Stop()  // leaves the window
+            case allegro.MouseWarpedEvent:           log("MouseWarpedEvent"          );
+
+            // Timer Events
+            case allegro.TimerEvent:                 log("TimerEvent:", timer.Count())
+                // Cycle background color for testing
+                func(color byte){
+                    allegro.ClearToColor(allegro.MapRGB(1*color&0xFF,2*color&0xFF,3*color&0xFF))
+                    allegro.FlipDisplay()
+                }(byte(timer.Count()))
+
+            // User Events
+            case allegro.UserEvent:                  log("UserEvent"                 );
+
+            default:                                 log("UnknownEvent:", e          );
             }
         }
-    })
+    }) // allegro.Run(func(){...})
 }
 
