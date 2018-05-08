ae6d35feb24f148183f45150205ff47861acd693
Samuel Archer <desearcher@gmail.com>
2018-05-08 05:32:14 -0400

    fixed event source issue

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-    "fmt"
     "github.com/dradtke/go-allegro/allegro"
     "github.com/dradtke/go-allegro/allegro/audio"
     "github.com/dradtke/go-allegro/allegro/acodec"
@@ -49,26 +48,6 @@ func main() {
             err        error
         )
 
-        /* ////////////////
-        ** Install Keyboard
-        */ ////////////////
-        println("INSTALL KEYBOARD")
-        if err = allegro.InstallKeyboard(); err != nil {
-            panic(err)
-        } else {
-            defer logFunc("UNINSTALL KEYBOARD", allegro.UninstallKeyboard)
-        }
-        
-        /* ////////////////
-        ** Install Joystick
-        */ ////////////////
-        println("INSTALL JOYSTICK")
-        if err = allegro.InstallJoystick(); err != nil {
-            panic(err)
-        } else {
-            defer logFunc("UNINSTALL JOYSTICK", allegro.UninstallJoystick)
-        }
-        
         /* ///////////////////////////
         ** Install Audio & Audio Codec
         */ ///////////////////////////
@@ -98,15 +77,83 @@ func main() {
         ** Register Display
         */ ////////////////
         println("CREATE DISPLAY")
-        allegro.SetNewDisplayFlags(allegro.WINDOWED)
+        var flags allegro.DisplayFlags = 0
+
+        // flags |= allegro.WINDOWED
+        // flags |= allegro.FULLSCREEN
+        // flags |= allegro.OPENGL
+        // flags |= allegro.RESIZABLE
+        // flags |= allegro.FRAMELESS
+        // flags |= allegro.NOFRAME
+        // flags |= allegro.GENERATE_EXPOSE_EVENTS
+        // flags |= allegro.OPENGL_3_0
+        // flags |= allegro.OPENGL_FORWARD_COMPATIBLE
+        // flags |= allegro.FULLSCREEN_WINDOW
+
+        if flags == 0 {            println(" = DEFAULT"                  ) } else {
+            if flags & 0x001 > 0 { println(" = WINDOWED"                 ) }
+            if flags & 0x002 > 0 { println(" = FULLSCREEN"               ) }
+            if flags & 0x004 > 0 { println(" = OPENGL"                   ) }
+            if flags & 0x010 > 0 { println(" = RESIZABLE"                ) }
+            if flags & 0x020 > 0 { println(" = FRAMELESS"                ) }
+            if flags & 0x020 > 0 { println(" = NOFRAME"                  ) }
+            if flags & 0x040 > 0 { println(" = GENERATE_EXPOSE_EVENTS"   ) }
+            if flags & 0x080 > 0 { println(" = OPENGL_3_0"               ) }
+            if flags & 0x100 > 0 { println(" = OPENGL_FORWARD_COMPATIBLE") }
+            if flags & 0x200 > 0 { println(" = FULLSCREEN_WINDOW"        ) }
+        }
+
+        allegro.SetNewDisplayFlags(flags)
         if display, err = allegro.CreateDisplay(WINX,WINY); err != nil {
             panic(err)
         } else {
             defer logFunc("DESTROY DISPLAY", display.Destroy)
             display.SetWindowTitle("Game")
             eventQueue.Register(display)
+            eventQueue.RegisterEventSource(display.EventSource()) // Redundant?
         }
 
+        /* /////////////////
+        ** Register Joystick
+        */ /////////////////
+        println("INSTALL JOYSTICK")
+        if err = allegro.InstallJoystick(); err != nil {
+            panic(err)
+        } else {
+            defer logFunc("UNINSTALL JOYSTICK", allegro.UninstallJoystick)
+            eventQueue.RegisterEventSource(allegro.JoystickEventSource())
+        }
+        
+        /* /////////////////
+        ** Register Keyboard
+        */ /////////////////
+        println("INSTALL KEYBOARD")
+        if err = allegro.InstallKeyboard(); err != nil {
+            panic(err)
+        } else {
+            defer logFunc("UNINSTALL KEYBOARD", allegro.UninstallKeyboard)
+            if keyboard, err := allegro.KeyboardEventSource(); err != nil {
+                panic(err)
+            } else {
+                eventQueue.RegisterEventSource(keyboard)
+            }
+        }
+        
+        /* //////////////
+        ** Register Mouse
+        */ //////////////
+        println("INSTALL MOUSE")
+        if err = allegro.InstallMouse(); err != nil {
+            panic(err)
+        } else {
+            defer logFunc("UNINSTALL MOUSE", allegro.UninstallMouse)
+            if mouse, err := allegro.MouseEventSource(); err != nil {
+                panic(err)
+            } else {
+                eventQueue.RegisterEventSource(mouse)
+            }
+        }
+        
         /* //////////////
         ** Register Timer
         */ //////////////
@@ -137,31 +184,31 @@ func main() {
         ** Main Loop
         */ /////////
         for running {
-            switch e := eventQueue.WaitForEvent(&event); e.(type) { // TODO ////////////////////////////////////////////////////////////////////
-            //case allegro.JoystickAxisEvent:          println("JoystickAxisEvent")          ;
-            //case allegro.JoystickButtonDownEvent:    println("JoystickButtonDownEvent")    ; // Yes, This is a mess right now.
-            //case allegro.JoystickButtonUpEvent:      println("JoystickButtonUpEvent")      ; // 
-            //case allegro.JoystickConfigurationEvent: println("JoystickConfigurationEvent") ; // Trying to work out why the only events working
-            //case allegro.KeyDownEvent:               println("KeyDownEvent")               ; // right now are TimerEvent and DisplayCloseEvent
-            //case allegro.KeyUpEvent:                 println("KeyUpEvent")                 ; // 
-            //case allegro.KeyCharEvent:               println("KeyCharEvent")               ; // Timer and Display are eventQueue.Register()ed
-            //case allegro.MouseAxesEvent:             println("MouseAxesEvent")             ; // Still, other Display Events are not catching
-            //case allegro.MouseButtonDownEvent:       println("MouseButtonDownEvent")       ; // 
-            //case allegro.MouseButtonUpEvent:         println("MouseButtonUpEvent")         ; // Currently using Ratpoison as my win manager
-            //case allegro.MouseWarpedEvent:           println("MouseWarpedEvent")           ; // Will need to test on another wm to make sure
-            //case allegro.MouseEnterDisplayEvent:     println("MouseEnterDisplayEvent")     ;
-            //case allegro.MouseLeaveDisplayEvent:     println("MouseLeaveDisplayEvent")     ;
-            case allegro.TimerEvent:                 print("TimerEvent: ")                 ; println(timer.Count())
-            //case allegro.DisplayExposeEvent:         println("DisplayExposeEvent")         ;
-            //case allegro.DisplayResizeEvent:         println("DisplayResizeEvent")         ;
-            case allegro.DisplayCloseEvent:          println("DisplayCloseEvent")          ; running = false
-            //case allegro.DisplayLostEvent:           println("DisplayLostEvent")           ;
-            //case allegro.DisplayFoundEvent:          println("DisplayFoundEvent")          ;
-            //case allegro.DisplaySwitchOutEvent:      println("DisplaySwitchOutEvent")      ;
-            //case allegro.DisplaySwitchInEvent:       println("DisplaySwitchInEvent")       ;
-            //case allegro.DisplayOrientationEvent:    println("DisplayOrientationEvent")    ; // Defaulting to printing e.type until I can see
-            //case allegro.UserEvent:                  println("UserEvent")                  ; // Proper events triggering
-            default:                                 fmt.Printf("T\n", e)                  ;
+            switch e := eventQueue.WaitForEvent(&event); e.(type) {
+            case allegro.DisplayCloseEvent:          println("DisplayCloseEvent"         ); running = false
+            case allegro.DisplayExposeEvent:         println("DisplayExposeEvent"        );
+            case allegro.DisplayFoundEvent:          println("DisplayFoundEvent"         );
+            case allegro.DisplayLostEvent:           println("DisplayLostEvent"          );
+            case allegro.DisplayOrientationEvent:    println("DisplayOrientationEvent"   );
+            case allegro.DisplayResizeEvent:         println("DisplayResizeEvent"        );
+            case allegro.DisplaySwitchInEvent:       println("DisplaySwitchInEvent"      );
+            case allegro.DisplaySwitchOutEvent:      println("DisplaySwitchOutEvent"     );
+            case allegro.JoystickAxisEvent:          println("JoystickAxisEvent"         );
+            case allegro.JoystickButtonDownEvent:    println("JoystickButtonDownEvent"   );
+            case allegro.JoystickButtonUpEvent:      println("JoystickButtonUpEvent"     );
+            case allegro.JoystickConfigurationEvent: println("JoystickConfigurationEvent");
+            case allegro.KeyCharEvent:               println("KeyCharEvent"              );
+            case allegro.KeyDownEvent:               println("KeyDownEvent"              );
+            case allegro.KeyUpEvent:                 println("KeyUpEvent"                );
+            case allegro.MouseAxesEvent:             println("MouseAxesEvent"            );
+            case allegro.MouseButtonDownEvent:       println("MouseButtonDownEvent"      );
+            case allegro.MouseButtonUpEvent:         println("MouseButtonUpEvent"        );
+            case allegro.MouseEnterDisplayEvent:     println("MouseEnterDisplayEvent"    );
+            case allegro.MouseLeaveDisplayEvent:     println("MouseLeaveDisplayEvent"    );
+            case allegro.MouseWarpedEvent:           println("MouseWarpedEvent"          );
+            case allegro.TimerEvent:                 print(  "TimerEvent: "              ); println(timer.Count())
+            case allegro.UserEvent:                  println("UserEvent"                 );
+            default:                                 print(  "UnknownEvent: "            ); println(e)
             }
         }
     })
